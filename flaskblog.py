ef47adf7d3a47d271341fcbf7a6544c8c3e42d62
Alec Wells <alec.wells47@gmail.com>
2020-06-16 12:58:40 -0600

    Import flash and create flash message for registration

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -31,6 +31,9 @@ def about():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit()
+        flash(f'Account created for  {form.username.data}!', 'success')
+
     return render_template('register.html', title='Register', form=form)
 
 @app.route('/login')
