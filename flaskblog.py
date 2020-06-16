bdea71cdfd6209ff2ee8388aa8d25a438878563c
Alec Wells <alec.wells47@gmail.com>
2020-06-16 12:09:13 -0600

    Initial commit

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Alec Wells',
+        'title': 'Blog Post 1',
+        'content': 'First content',
+        'date_posted': 'June 9, 2020'
+    },
+    {
+        'author': 'Alec Wellsssss',
+        'title': 'Blog Post 1ssss',
+        'content': 'First contentsssss',
+        'date_posted': 'June 9, 2020sssss'
+    }
+]
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
