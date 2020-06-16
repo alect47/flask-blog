bd782e44c4245be587de8b324273885481aa037d
Alec Wells <alec.wells47@gmail.com>
2020-06-16 12:19:46 -0600

    Add validators to form

--- forms.py
+++ forms.py
@@ -1,5 +1,12 @@
 from flask_wtf import FlaskFormgit
 from wtforms import StringField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username')
+    username = StringField('Username',
+        validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+        validators=[DataRequired(), EqualTo('passwordg')])
