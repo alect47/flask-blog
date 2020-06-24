bcca16ba15e62979cd4adfe9ba50ea86878d984e
Alec Wells <alec.wells47@gmail.com>
2020-06-24 13:55:14 -0600

    Add flask_login

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -18,6 +18,11 @@ class RegistrationForm(FlaskForm):
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
 
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
