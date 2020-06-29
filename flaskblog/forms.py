209f8186443c8fe67ab37fe93dd9b92a8daeb99f
Alec Wells <alec.wells47@gmail.com>
2020-06-29 15:16:30 -0600

    Fix typo in forms.py

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -46,7 +46,7 @@ class UpdateAccountForm(FlaskForm):
                 raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
-        if email.data != current_user.emai;:
+        if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is taken. Please choose a different one.')
