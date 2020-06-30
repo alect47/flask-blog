1ca2da5394d8ccd81ba81f9cd2e3c6aa245abc33
Alec Wells <alec.wells47@gmail.com>
2020-06-30 15:44:16 -0600

    Fix typo in /post/new route

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -96,7 +96,8 @@ def new_post():
     if form.validate_on_submit():
         post = Post(title=form.title.data, content=form.content.data, author=current_user)
         db.session.add(post)
-        db.session.commit
+        db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='New Post')
