176b04e63a09e95eb8a1b73e239e8ba44a98e194
Alec Wells <alec.wells47@gmail.com>
2020-07-14 11:31:37 -0600

    Fix typo in init_py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -13,11 +13,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-app.congig['MAIL_SERVER'] = 'smtp.googleemail.com'
-app.congig['MAIL_PORT'] = 587
-app.congig['MAIL_USER_TLS'] = True
-app.congig['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.congig['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config['MAIL_SERVER'] = 'smtp.googleemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USER_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
 
